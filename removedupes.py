da3c1ebb3646d47e8edf68b10a8e4b7a699bacf3
nickesparza <nickesparza@gmail.com>
2022-10-04 15:33:01 -0500

    add comments to removedupes

--- removedupes.py
+++ removedupes.py
@@ -1,6 +1,10 @@
+# task: remove duplicates in a sorted array
 def removeDuplicates(nums):
     for num in nums:
+        # leverage the list function 'count' to establish condition for while loop
         while nums.count(num) > 1:
+            # while there are any more than one of a given number in the list, remove that number
+            # because the list is sorted, no need for further specificity
             nums.remove(num)
     return nums
 
