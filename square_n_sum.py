33050199b944b1169d602a2dcf1728179c16d84c
nickesparza <nickesparza@gmail.com>
2022-10-04 15:30:48 -0500

    add comments to square_n_sum

--- square_n_sum.py
+++ square_n_sum.py
@@ -1,9 +1,11 @@
 def square_sum(numbers):
-    #your code here
+    # loop over enumerated list to access the index and modify it
     for index, number in enumerate(numbers):
+        # perform the square operation
         numbers[index] = number ** 2
         print(number)
     print(numbers)
+    # use the built-in sum function to return the sum of all list elements
     return sum(numbers)
 
 print(square_sum([0, 3, 4, 5]))
